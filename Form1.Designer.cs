212530057217e6da5761e10c37d1f8db6ce3f2c6
Nathan Stéwill LOUFOUA <127401929+LNStewill@users.noreply.github.com>
2024-05-22 22:25:01 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace gestiondvd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstDvd = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAjoutDvd = new System.Windows.Forms.Button();
+            this.btnSupprDvd = new System.Windows.Forms.Button();
+            this.btnModifDvd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDurée = new System.Windows.Forms.TextBox();
+            this.txtTitre = new System.Windows.Forms.TextBox();
+            this.cboGenre = new System.Windows.Forms.ComboBox();
+            this.btnSupprGenre = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstActeur = new System.Windows.Forms.ListBox();
+            this.txtAjoutGenre = new System.Windows.Forms.TextBox();
+            this.btnAjoutGenre = new System.Windows.Forms.Button();
+            this.btnAjoutActeurDvd = new System.Windows.Forms.Button();
+            this.btnSupprActeurDvd = new System.Windows.Forms.Button();
+            this.cboActeur = new System.Windows.Forms.ComboBox();
+            this.btnSupprActeur = new System.Windows.Forms.Button();
+            this.txtAjoutActeur = new System.Windows.Forms.TextBox();
+            this.btnAjoutActeur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(42, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(404, 101);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TESTING MYSQL CONNECTION";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstDvd
+            // 
+            this.lstDvd.FormattingEnabled = true;
+            this.lstDvd.ItemHeight = 16;
+            this.lstDvd.Location = new System.Drawing.Point(10, 194);
+            this.lstDvd.Name = "lstDvd";
+            this.lstDvd.Size = new System.Drawing.Size(301, 372);
+            this.lstDvd.TabIndex = 1;
+            this.lstDvd.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "lst des Dvd";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnAjoutDvd
+            // 
+            this.btnAjoutDvd.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAjoutDvd.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnAjoutDvd.FlatAppearance.BorderSize = 0;
+            this.btnAjoutDvd.Location = new System.Drawing.Point(317, 165);
+            this.btnAjoutDvd.Name = "btnAjoutDvd";
+            this.btnAjoutDvd.Size = new System.Drawing.Size(202, 23);
+            this.btnAjoutDvd.TabIndex = 3;
+            this.btnAjoutDvd.Text = "Ajout Dvd (sans les acteurs)";
+            this.btnAjoutDvd.UseVisualStyleBackColor = false;
+            // 
+            // btnSupprDvd
+            // 
+            this.btnSupprDvd.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSupprDvd.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSupprDvd.FlatAppearance.BorderSize = 0;
+            this.btnSupprDvd.Location = new System.Drawing.Point(317, 197);
+            this.btnSupprDvd.Name = "btnSupprDvd";
+            this.btnSupprDvd.Size = new System.Drawing.Size(202, 23);
+            this.btnSupprDvd.TabIndex = 4;
+            this.btnSupprDvd.Text = "Suppr Dvd";
+            this.btnSupprDvd.UseVisualStyleBackColor = false;
+            // 
+            // btnModifDvd
+            // 
+            this.btnModifDvd.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnModifDvd.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnModifDvd.FlatAppearance.BorderSize = 0;
+            this.btnModifDvd.Location = new System.Drawing.Point(317, 226);
+            this.btnModifDvd.Name = "btnModifDvd";
+            this.btnModifDvd.Size = new System.Drawing.Size(202, 23);
+            this.btnModifDvd.TabIndex = 5;
+            this.btnModifDvd.Text = "Modif (titre durée genre)";
+            this.btnModifDvd.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(535, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Titre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Durée";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Genre";
+            // 
+            // txtDurée
+            // 
+            this.txtDurée.Location = new System.Drawing.Point(585, 194);
+            this.txtDurée.Name = "txtDurée";
+            this.txtDurée.Size = new System.Drawing.Size(183, 22);
+            this.txtDurée.TabIndex = 9;
+            // 
+            // txtTitre
+            // 
+            this.txtTitre.Location = new System.Drawing.Point(585, 162);
+            this.txtTitre.Name = "txtTitre";
+            this.txtTitre.Size = new System.Drawing.Size(401, 22);
+            this.txtTitre.TabIndex = 10;
+            // 
+            // cboGenre
+            // 
+            this.cboGenre.FormattingEnabled = true;
+            this.cboGenre.Location = new System.Drawing.Point(585, 225);
+            this.cboGenre.Name = "cboGenre";
+            this.cboGenre.Size = new System.Drawing.Size(291, 24);
+            this.cboGenre.TabIndex = 11;
+            // 
+            // btnSupprGenre
+            // 
+            this.btnSupprGenre.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSupprGenre.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSupprGenre.FlatAppearance.BorderSize = 0;
+            this.btnSupprGenre.Location = new System.Drawing.Point(882, 222);
+            this.btnSupprGenre.Name = "btnSupprGenre";
+            this.btnSupprGenre.Size = new System.Drawing.Size(104, 23);
+            this.btnSupprGenre.TabIndex = 12;
+            this.btnSupprGenre.Text = "Suppr Genre";
+            this.btnSupprGenre.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(535, 311);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Acteurs";
+            // 
+            // lstActeur
+            // 
+            this.lstActeur.FormattingEnabled = true;
+            this.lstActeur.ItemHeight = 16;
+            this.lstActeur.Location = new System.Drawing.Point(592, 311);
+            this.lstActeur.Name = "lstActeur";
+            this.lstActeur.Size = new System.Drawing.Size(284, 180);
+            this.lstActeur.TabIndex = 14;
+            // 
+            // txtAjoutGenre
+            // 
+            this.txtAjoutGenre.Location = new System.Drawing.Point(588, 260);
+            this.txtAjoutGenre.Name = "txtAjoutGenre";
+            this.txtAjoutGenre.Size = new System.Drawing.Size(288, 22);
+            this.txtAjoutGenre.TabIndex = 15;
+            // 
+            // btnAjoutGenre
+            // 
+            this.btnAjoutGenre.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAjoutGenre.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnAjoutGenre.FlatAppearance.BorderSize = 0;
+            this.btnAjoutGenre.Location = new System.Drawing.Point(882, 257);
+            this.btnAjoutGenre.Name = "btnAjoutGenre";
+            this.btnAjoutGenre.Size = new System.Drawing.Size(104, 23);
+            this.btnAjoutGenre.TabIndex = 16;
+            this.btnAjoutGenre.Text = "Ajout Genre";
+            this.btnAjoutGenre.UseVisualStyleBackColor = false;
+            // 
+            // btnAjoutActeurDvd
+            // 
+            this.btnAjoutActeurDvd.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAjoutActeurDvd.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnAjoutActeurDvd.FlatAppearance.BorderSize = 0;
+            this.btnAjoutActeurDvd.Location = new System.Drawing.Point(882, 311);
+            this.btnAjoutActeurDvd.Name = "btnAjoutActeurDvd";
+            this.btnAjoutActeurDvd.Size = new System.Drawing.Size(32, 23);
+            this.btnAjoutActeurDvd.TabIndex = 17;
+            this.btnAjoutActeurDvd.Text = "+";
+            this.btnAjoutActeurDvd.UseVisualStyleBackColor = false;
+            // 
+            // btnSupprActeurDvd
+            // 
+            this.btnSupprActeurDvd.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSupprActeurDvd.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSupprActeurDvd.FlatAppearance.BorderSize = 0;
+            this.btnSupprActeurDvd.Location = new System.Drawing.Point(882, 340);
+            this.btnSupprActeurDvd.Name = "btnSupprActeurDvd";
+            this.btnSupprActeurDvd.Size = new System.Drawing.Size(32, 23);
+            this.btnSupprActeurDvd.TabIndex = 18;
+            this.btnSupprActeurDvd.Text = "-";
+            this.btnSupprActeurDvd.UseVisualStyleBackColor = false;
+            // 
+            // cboActeur
+            // 
+            this.cboActeur.FormattingEnabled = true;
+            this.cboActeur.Location = new System.Drawing.Point(594, 507);
+            this.cboActeur.Name = "cboActeur";
+            this.cboActeur.Size = new System.Drawing.Size(282, 24);
+            this.cboActeur.TabIndex = 19;
+            // 
+            // btnSupprActeur
+            // 
+            this.btnSupprActeur.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSupprActeur.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSupprActeur.FlatAppearance.BorderSize = 0;
+            this.btnSupprActeur.Location = new System.Drawing.Point(882, 506);
+            this.btnSupprActeur.Name = "btnSupprActeur";
+            this.btnSupprActeur.Size = new System.Drawing.Size(104, 23);
+            this.btnSupprActeur.TabIndex = 20;
+            this.btnSupprActeur.Text = "Suppr acteur";
+            this.btnSupprActeur.UseVisualStyleBackColor = false;
+            // 
+            // txtAjoutActeur
+            // 
+            this.txtAjoutActeur.Location = new System.Drawing.Point(595, 543);
+            this.txtAjoutActeur.Name = "txtAjoutActeur";
+            this.txtAjoutActeur.Size = new System.Drawing.Size(281, 22);
+            this.txtAjoutActeur.TabIndex = 21;
+            // 
+            // btnAjoutActeur
+            // 
+            this.btnAjoutActeur.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAjoutActeur.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnAjoutActeur.FlatAppearance.BorderSize = 0;
+            this.btnAjoutActeur.Location = new System.Drawing.Point(882, 543);
+            this.btnAjoutActeur.Name = "btnAjoutActeur";
+            this.btnAjoutActeur.Size = new System.Drawing.Size(104, 23);
+            this.btnAjoutActeur.TabIndex = 22;
+            this.btnAjoutActeur.Text = "Ajout acteur";
+            this.btnAjoutActeur.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 600);
+            this.Controls.Add(this.btnAjoutActeur);
+            this.Controls.Add(this.txtAjoutActeur);
+            this.Controls.Add(this.btnSupprActeur);
+            this.Controls.Add(this.cboActeur);
+            this.Controls.Add(this.btnSupprActeurDvd);
+            this.Controls.Add(this.btnAjoutActeurDvd);
+            this.Controls.Add(this.btnAjoutGenre);
+            this.Controls.Add(this.txtAjoutGenre);
+            this.Controls.Add(this.lstActeur);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSupprGenre);
+            this.Controls.Add(this.cboGenre);
+            this.Controls.Add(this.txtTitre);
+            this.Controls.Add(this.txtDurée);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnModifDvd);
+            this.Controls.Add(this.btnSupprDvd);
+            this.Controls.Add(this.btnAjoutDvd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDvd);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Gestion des DVD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstDvd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAjoutDvd;
+        private System.Windows.Forms.Button btnSupprDvd;
+        private System.Windows.Forms.Button btnModifDvd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDurée;
+        private System.Windows.Forms.TextBox txtTitre;
+        private System.Windows.Forms.ComboBox cboGenre;
+        private System.Windows.Forms.Button btnSupprGenre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox lstActeur;
+        private System.Windows.Forms.TextBox txtAjoutGenre;
+        private System.Windows.Forms.Button btnAjoutGenre;
+        private System.Windows.Forms.Button btnAjoutActeurDvd;
+        private System.Windows.Forms.Button btnSupprActeurDvd;
+        private System.Windows.Forms.ComboBox cboActeur;
+        private System.Windows.Forms.Button btnSupprActeur;
+        private System.Windows.Forms.TextBox txtAjoutActeur;
+        private System.Windows.Forms.Button btnAjoutActeur;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestiondvd
+{
+    
+
+    public partial class Form1 : Form
+    {
+        class ConnexionSql
+        {
+            // propriétés
+            private bool finCurseur = true; // fin du curseur atteinte
+            private MySqlConnection connection; // chaine de connexion
+            private MySqlCommand command; // envoi de la requête à la base de données
+            private MySqlDataReader reader; // gestion du curseur
+
+            // constructeur
+            public ConnexionSql(string chaineConnection)
+            {
+                this.connection = new MySqlConnection(chaineConnection);
+                this.connection.Open();
+            }
+
+            // execution d'une requete select
+            public void reqSelect(string chaineRequete)
+            {
+                this.command = new MySqlCommand(chaineRequete, this.connection);
+                this.reader = this.command.ExecuteReader();
+                this.finCurseur = false;
+                this.suivant();
+            }
+
+            // execution d'une requete update
+            public void reqUpdate(string chaineRequete)
+            {
+                this.command = new MySqlCommand(chaineRequete, this.connection);
+                this.command.ExecuteNonQuery();
+                this.finCurseur = true;
+            }
+
+            // récupération d'un champ
+            public Object champ(string nomChamp)
+            {
+                return this.reader[nomChamp];
+            }
+
+            // passage à la ligne suivante du curseur
+            public void suivant()
+            {
+                if (!this.finCurseur)
+                {
+                    this.finCurseur = !this.reader.Read();
+                }
+            }
+
+            // test de la fin du curseur
+            public Boolean fin()
+            {
+                return this.finCurseur;
+            }
+
+            // fermeture de la connexion
+            public void close()
+            {
+                this.connection.Close();
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string chaineConnection = null;
+            MySqlConnection cnn;
+            chaineConnection = "server=localhost;database=gestiondvd;uid=root;pwd=\"\";";
+            cnn = new MySqlConnection(chaineConnection);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Connexion à la base de données
+            string chaineConnection = "server=localhost;database=gestiondvd;uid=root;pwd=\"\";";
+            ConnexionSql connexion = new ConnexionSql(chaineConnection);
+
+            // Exécution de la requête SELECT
+            connexion.reqSelect("SELECT titre FROM dvd");
+
+            // Ajout des éléments à la ListBox
+            while (!connexion.fin())
+            {
+                string item = connexion.champ("titre").ToString();
+                lstDvd.Items.Add(item);
+                connexion.suivant();
+            }
+
+            // Fermeture de la connexion
+            connexion.close();
+
+           /* // Connexion pour Genre
+            ConnexionSql connexionGenre = new ConnexionSql(chaineConnection);
+            connexion.reqSelect("SELECT libelle FROM genre");
+            while (connexionGenre.fin())
+            {
+                *//*this.cboGenre.Items.Add((connexion.champ("libelle"))).ToString();
+                connexion.suivant();*/
+
+               /* string item = connexionGenre.champ("libelle").ToString();
+                cboGenre.Items.Add(item);
+                connexionGenre.suivant();
+            }
+            connexionGenre.close();
+
+
+            // Connexion pour Acteur
+            ConnexionSql connexionActeur = new ConnexionSql(chaineConnection);
+            connexion.reqSelect("SELECT libelle FROM genre");
+            while (connexionActeur.fin())
+            {
+             */   /*this.cboGenre.Items.Add((connexion.champ("libelle"))).ToString();
+                connexion.suivant();*/
+            /*
+                string item = connexionActeur.champ("libelle").ToString();
+                cboGenre.Items.Add(item);
+                connexionActeur.suivant();
+            }
+            connexionActeur.close();*/
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
